291da9b1227bbc703920de7186bd4eafbf9bbdd8
PixelWolf75 <95386369+PixelWolf75@users.noreply.github.com>
2024-10-19 23:26:34 +0100

    Made the slider move to an empty space when clicked on

--- Assets/scripts/quadrant control.cs
+++ Assets/scripts/quadrant control.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class quadrantcontrol : MonoBehaviour
 {
+    [SerializeField] private GameObject emptySpace;
+    private Camera _camera;
     public Vector2 MoveValue;
     public float fMouseX = 0.00f;
     public float fMouseY = 0.00f;
@@ -11,6 +13,7 @@ public class quadrantcontrol : MonoBehaviour
     void Start()
     {
         Debug.Log("Quadrant Control script has started");
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
@@ -19,7 +22,20 @@ public class quadrantcontrol : MonoBehaviour
         // Registers every frame the mouse button is held down
         if (Input.GetMouseButton(0))
         {
-            Debug.Log("Mouse is held down");
+            //Debug.Log("Mouse is held down");
+
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(Vector3.Distance(emptySpace.transform.position, hit.transform.position));
+                if(Vector3.Distance(emptySpace.transform.position, hit.transform.position) < 20)
+                {
+                    Vector3 lastEmptySpacePos = emptySpace.transform.position;
+                    emptySpace.transform.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpacePos;
+                }
+            }
 
             fMouseX = Input.GetAxis("Mouse X");
             fMouseY = Input.GetAxis("Mouse Y");
