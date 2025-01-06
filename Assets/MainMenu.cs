a20f50b68ff444541f93207f58982b11c179287e
JustinMirto <119904330+JustinMirto@users.noreply.github.com>
2025-01-06 07:36:09 +0000

    Revert "Merge branch 'main' of https://github.com/JustinMirto/quadrants"
    This reverts commit 8255e2d6914f7659f3db639da5589585f2fd48bf, reversing
    changes made to 3a4b1c6ba2e76590571b7b5c517e7863180e1446.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,38 +6,25 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     SoundManager soundManager;
-
     private void Awake()
     {
         soundManager = GameObject.FindGameObjectWithTag("Sound").GetComponent<SoundManager>();
-    }
 
-    public void PlayGame()
-    {
-        // Load the next scene (e.g., Level 1)
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void PlayGame ()
+   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
 
-    public void LoadLevel2()
-    {
-        // Load Level 2
-        SceneManager.LoadScene(2);
-    }
+   public void QuitGame ()
+   {
+      Debug.Log("QUIT");
+      Application.Quit();
+   }
 
-    public void LoadLevel3()
-    {
-        // Load Level 3
-        SceneManager.LoadScene(3);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("QUIT");
-        Application.Quit();
-    }
-
-    public void pressButton()
+    public void pressButton() 
     {
         soundManager.playSoundEffects(soundManager.uiSound);
+
     }
 }
