078d070647ff156b95e7ada6c7ad87e9b3658222
JustinMirto <119904330+JustinMirto@users.noreply.github.com>
2025-01-06 08:56:07 +0000

    clean up and bug

--- Assets/Scripts/Functional/MainMenu.cs
+++ Assets/Scripts/Functional/MainMenu.cs
@@ -6,25 +6,38 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     SoundManager soundManager;
+
     private void Awake()
     {
         soundManager = GameObject.FindGameObjectWithTag("Sound").GetComponent<SoundManager>();
-
     }
-    public void PlayGame ()
-   {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
 
-   public void QuitGame ()
-   {
-      Debug.Log("QUIT");
-      Application.Quit();
-   }
+    public void PlayGame()
+    {
+        // Load the next scene (e.g., Level 1)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-    public void pressButton() 
+    public void LoadLevel2()
+    {
+        // Load Level 2
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel3()
+    {
+        // Load Level 3
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void pressButton()
     {
         soundManager.playSoundEffects(soundManager.uiSound);
-
     }
 }
